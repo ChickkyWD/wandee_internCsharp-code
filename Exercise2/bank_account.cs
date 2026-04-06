9ce11ad89a1a2215081f1298d7869914ab9d152d
ChickkyWD <chikkywd@gmail.com>
2026-04-06 18:57:39 +0700

    Update Exercise2

--- Exercise2/bank_account.cs
+++ Exercise2/bank_account.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+
 class Program
 {
     static void Main()
     {
-        int balance = 100;
+        int balance = 0;
         int choice;
+
+        // First deposit (must be >= 100)
+        while (balance < 100)
+        {
+            int firstDeposit;
+
+            while (true)
+            {
+                Console.Write("Please deposit at least 100 Bath to proceed with other transactions: ");
+
+                if (int.TryParse(Console.ReadLine(), out firstDeposit) && firstDeposit >= 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input! Please enter a positive number.\n");
+            }
+
+            if (firstDeposit >= 100)
+            {
+                balance += firstDeposit;
+                Console.WriteLine("Success! Account balance is : " + balance + " Bath\n");
+            }
+            else
+            {
+                Console.WriteLine("You must deposit at least 100 Bath\n");
+            }
+        }
 
         do
         {
@@ -15,29 +43,54 @@ class Program
             Console.WriteLine("4. Exit");
             Console.Write("Select Number : ");
 
-            choice = int.Parse(Console.ReadLine());
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid menu selection.\n");
+                continue;
+            }
 
             switch (choice)
             {
                 case 1: // Deposit
-                    Console.Write("Enter money to deposit : ");
-                    int deposit = int.Parse(Console.ReadLine());
+                    int deposit;
+                    while (true)
+                    {
+                        Console.Write("Enter money to deposit : ");
+                        if (int.TryParse(Console.ReadLine(), out deposit) && deposit > 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Invalid input! Please enter a number greater than 0");
+                    }
+
                     balance += deposit;
                     Console.WriteLine("Success! Account balance is : " + balance + " Bath");
                     break;
 
                 case 2: // Withdraw
-                    Console.Write("Enter money to withdraw : ");
-                    int withdraw = int.Parse(Console.ReadLine());
-
-                    if (balance - withdraw >= 100)
+                    int withdraw;
+                    while (true)
                     {
-                        balance -= withdraw;
-                        Console.WriteLine("Success! Account balance is : " + balance + " Bath");
+                        Console.Write("Enter money to withdraw : ");
+                        if (int.TryParse(Console.ReadLine(), out withdraw) && withdraw > 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Invalid input! Please enter a number greater than 0");
+                    }
+
+                    if (withdraw > balance)
+                    {
+                        Console.WriteLine("Can not withdraw because money is not enough");
+                    }
+                    else if (balance - withdraw < 100)
+                    {
+                        Console.WriteLine("Can not withdraw because balance must be at least 100 Bath");
                     }
                     else
                     {
-                        Console.WriteLine("Can not withdraw because money will less than 100 Bath");
+                        balance -= withdraw;
+                        Console.WriteLine("Success! Account balance is : " + balance + " Bath");
                     }
                     break;
 
